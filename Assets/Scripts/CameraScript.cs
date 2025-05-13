def09c4c2f8d4e9fef3902300074fa37121da200
umutucak <umutucak@proton.me>
2025-05-13 18:00:53 +0200

    cool raycasts + momo interaction hint textbox

--- Assets/Scripts/CameraScript.cs
+++ Assets/Scripts/CameraScript.cs
@@ -4,6 +4,7 @@ public class CameraScript : MonoBehaviour
 {
     Ray ray;
     Camera camera;
+    public GameObject fuckingMonkey;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -13,17 +14,30 @@ public class CameraScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {   
-        Debug.Log(Input.GetMouseButtonDown(0));
+        RaycastHit hit;
+        ray = camera.ScreenPointToRay(Input.mousePosition);
+        // if left click
         if (Input.GetMouseButtonDown(0))
         {
-            Debug.Log("Pressed left-click.");
-            ray = camera.ScreenPointToRay(Input.mousePosition);
-            if (Physics.Raycast(ray, out RaycastHit hit, 10))
+            if (Physics.Raycast(ray, out hit, 5))
             {
                 Transform objectHit = hit.transform;
-                Debug.Log(objectHit);
             }
+            return;
+        }
 
+        // always passive raycasting
+        if (Physics.Raycast(ray, out hit))
+        {
+            if (hit.collider.name == "GreetingMonkey" && hit.distance < 5)
+            {
+                fuckingMonkey = hit.transform.Find("InteractionText").gameObject;
+                fuckingMonkey.SetActive(true);
+            }
+            else
+            {
+                fuckingMonkey.SetActive(false);
+            }
         }
     }
 }
